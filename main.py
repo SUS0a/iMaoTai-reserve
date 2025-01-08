068a13a51d05bd4f75ec7ed14c1346cd26593662
SUS0a <33076387+SUS0a@users.noreply.github.com>
2025-01-08 09:32:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import privateCrypt
 
 DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
 TODAY = datetime.date.today().strftime("%Y%m%d")
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.Error,
                     format='%(asctime)s  %(filename)s : %(levelname)s  %(message)s',  # 定义输出log的格式
                     stream=sys.stdout,
                     datefmt=DATE_FORMAT)
